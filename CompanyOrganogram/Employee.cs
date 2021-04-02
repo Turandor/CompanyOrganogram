f2bde49119e28a4ba3105a0dbb5a8de2571e7507
Turandor <senjin56@gmail.com>
2021-04-02 14:16:13 +0200

    Working organogram

---
+++ CompanyOrganogram/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyOrganogram
+{
+    public class Employee
+    {
+        public int id;
+        public int superiorId;
+        string name;
+        string surename;
+        string company;
+        string city;
+        string job;
+        string firstNumber;
+        string secondNumber;
+        string thirdNumber;
+
+        public Employee(int id, int superiorId, string name, string surename,
+                        string company, string city, string job,
+                        string firstNumber, string secondNumber, string thirdNumber)
+        {
+            this.id = id;
+            this.superiorId = superiorId;
+            this.name = name;
+            this.surename = surename;
+            this.company = company;
+            this.city = city;
+            this.job = job;
+            this.firstNumber = firstNumber;
+            this.secondNumber = secondNumber;
+            this.thirdNumber = thirdNumber;
+        }
+
+        public string PrintEmployee()
+        {
+            return (id + ", " + superiorId + ", " + name + ", " +
+                surename + ", " + company + ", " + city + ", " +
+                job);
+        }
+    }
+}
--- CompanyOrganogram/Program.cs
+++ CompanyOrganogram/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CompanyOrganogram
 {
@@ -10,6 +11,59 @@ namespace CompanyOrganogram
     {
         static void Main(string[] args)
         {
+            var path = Path.Combine(AppContext.BaseDirectory, "..\\companies_data.csv");
+            using (var reader = new StreamReader(path))
+            {
+                List<Employee> Employees = new List<Employee>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    Employees.Add(new Employee(int.Parse(values[0]), int.Parse(values[1]), 
+                                               values[2], values[3], values[4], values[5], 
+                                               values[6], values[7], values[8], values[9]));
+                }
+                List<Employee> sortedList = Employees.OrderBy(o => o.id).ToList();
+                PrintOrganogram(sortedList);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void PrintOrganogram(List<Employee> employees)
+        {
+            foreach (var item in employees)
+            {
+                if (item.superiorId == 0)
+                {
+                    //employees.Remove(item);
+                    Console.WriteLine(item.PrintEmployee());
+                    FindInferiors(item, employees, 1);
+                }
+
+            }
+        }
+        
+        static void FindInferiors(Employee superior, List<Employee> employees, int level)
+        {
+            foreach (var item in employees)
+            {
+                if (superior.id == item.superiorId)
+                {
+                    Console.WriteLine(ParagraphIndent(level) + item.PrintEmployee());
+                    //employees.Remove(item);
+                    FindInferiors(item, employees, level+1);
+                }
+            }
+        }
+        static string ParagraphIndent(int level)
+        {
+            string paragraphIndent = "";
+            for (int i = 0; i < level; i++)
+            {
+                paragraphIndent += "    ";
+            }
+            return paragraphIndent;
         }
     }
 }
